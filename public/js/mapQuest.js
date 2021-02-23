7aec9c5e63af5b6ba472d5474b3045179db0137c
kimtienvu <kvu@ucsd.edu>
2021-02-22 20:49:54 -0800

    lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'i90JQQYtYcbOr8lJt6f3AZz8MFpLim06';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+    center: [32.88, -117.236],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
