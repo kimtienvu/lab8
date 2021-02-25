5af3c6b67ba90bcd5e40e2c2d79e60123993689a
Kim Tien Vu <45075049+kimtienvu@users.noreply.github.com>
2021-02-25 09:23:30 -0800

    Update mapQuest.js

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -3,7 +3,7 @@ function initMap() {
 
 	// 'map' refers to a <div> element with the ID map
 	var map = L.mapquest.map('map', {
-    center: [32.88, -117.236],
+    center: [32.8785, -117.2359],
     layers: L.mapquest.tileLayer('map'),
     zoom: 12,
     zoomControl: false
